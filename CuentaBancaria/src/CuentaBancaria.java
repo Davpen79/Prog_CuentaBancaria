fc96010df26bbc2487d07ce9de2a08d59951f2e2
David Pena Fernández <a25davidpf@teis.intranet>
2025-10-01 21:10:13 +0200

    Programa CuentaBancaria completado

--- CuentaBancaria/src/CuentaBancaria.java
+++ CuentaBancaria/src/CuentaBancaria.java
@@ -5,26 +5,27 @@ public class CuentaBancaria {
     private String titular;
     private double saldo;
 
-    private double movimiento;
-
     //Getter y Setter
 
     public String getTitular() {
+
         return titular;
     }
 
     public void setTitular(String titular) {
+
         this.titular = titular;
     }
 
     public double getSaldo() {
+
         return saldo;
     }
 
     public void setSaldo(double saldo) {
+
         this.saldo = saldo;
     }
-
 
     //Constructores
 
@@ -33,20 +34,17 @@ public class CuentaBancaria {
         this.saldo = saldo;
     }
 
-
     //Funciones
 
-    public double depositarCash(){
-        return this.saldo + movimiento;
+    public double depositarCash(double dinero) {
+
+        this.saldo = saldo + dinero;
+        return this.saldo;
     }
 
-    public double retirarCash(){
+    public double retirarCash(double dinero) {
 
-        if((this.saldo - movimiento) < 0){
-            System.out.println("No hay saldo suficiente");
-        }
-
-        return this.saldo - movimiento;
-
+        this.saldo = saldo - dinero;
+        return  this.saldo;
     }
 }
--- CuentaBancaria/src/Program.java
+++ CuentaBancaria/src/Program.java
@@ -4,20 +4,49 @@ public class Program {
 
     public static void main(String[] args) {
 
-        enum Movimientos {RETIRADA, DEPOSITO}
-
-        System.out.println("Bienvenido a su Querido Banco.\n" +
-                "Por favor Ingrese el nombre del Titular de la cuenta.");
+        System.out.println("Bienvenido a su Querido Banco.\n" + "Por favor Ingrese el nombre del Titular de la cuenta.");
         Scanner sc = new Scanner(System.in);
         String titular = sc.nextLine();
         double saldo = 0;
 
-        CuentaBancaria cuentaUsuario = new CuentaBancaria(titular,0);
+        CuentaBancaria cuentaUsuario = new CuentaBancaria(titular, 0);
+
         titular = cuentaUsuario.getTitular();
         saldo = cuentaUsuario.getSaldo();
 
-        System.out.println("Hola " + titular);
-        System.out.println("El saldo de tu cuenta es de " + saldo + " euros");
+        int optionSelected = 1;
+
+        while (optionSelected != 3) {
+
+            System.out.println("Hola " + titular);
+            System.out.println("El saldo de tu cuenta es de " + saldo + " euros");
+            System.out.println("Que operacion deseas realizar?\n" + "1. Deposito\n" + "2. Retirada\n" + "3. Salida");
+
+            optionSelected = sc.nextInt();
+
+            if (optionSelected == 1) {
+                System.out.println("Cuantos euros quieres depositar");
+                Scanner sd = new Scanner(System.in);
+                double dinero = sd.nextDouble();
+                saldo = cuentaUsuario.depositarCash(dinero);
+                System.out.println("Tu saldo actual es " + saldo);
+            }
+            if (optionSelected == 2) {
+
+                System.out.println("Cuantos euros quieres retirar");
+                Scanner sd = new Scanner(System.in);
+                double dinero = sd.nextDouble();
+                if (dinero <= saldo) {
+                    saldo = cuentaUsuario.retirarCash(dinero);
+                    System.out.println("Tu saldo actual es " + saldo);
+                } else {
+                    System.out.println("Tu saldo actual no es suficiente para realizar esta operacion");
+                }
+            } else {
+                System.out.println("Que tengas un buen dia");
+            }
+        }
+
 
     }
 }
